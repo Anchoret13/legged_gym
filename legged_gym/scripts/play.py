0cb8693c44cfc7baf88221eb7959c24513db6897
Anchoret13 <vergil1133@live.com>
2023-11-13 16:53:57 -0600

    update

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -42,7 +42,7 @@ from moviepy.editor import ImageSequenceClip
 def play(args):
     env_cfg, train_cfg = task_registry.get_cfgs(name=args.task)
     # override some parameters for testing
-    env_cfg.env.num_envs = min(env_cfg.env.num_envs, 50)
+    env_cfg.env.num_envs = min(env_cfg.env.num_envs, 30)
     env_cfg.terrain.num_rows = 5
     env_cfg.terrain.num_cols = 5
     env_cfg.terrain.curriculum = False
@@ -81,7 +81,6 @@ def play(args):
         if RECORD_FRAMES:
             if i % 2:
                 filename = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', train_cfg.runner.experiment_name, 'exported', 'frames', f"{img_idx}.png")
-                print(filename)
                 env.gym.write_viewer_image_to_file(env.viewer, filename)
                 img_idx += 1 
         if MOVE_CAMERA:
